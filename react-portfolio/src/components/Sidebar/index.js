b60e8b47f0a7c5f1f4ffbd40dd083b341383c010
Amith <amith.tallanki@gmail.com>
2023-06-27 23:51:49 +0530

    Added changes for side nav bar
    Added changes for side nav bar

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,46 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUserAlt, faEnvelope, faBlog } from '@fortawesome/free-solid-svg-icons';
+import { faBloggerB, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img classname="sub-logo" src={LogoSubtitle} alt='slobadan' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUserAlt} color='#4d4d4e' />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/amithtallanki/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/GAT007'>
+                    <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://github.com/GAT007'>
+                    <FontAwesomeIcon icon={faBloggerB} color='#4d4d4e' />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar;
